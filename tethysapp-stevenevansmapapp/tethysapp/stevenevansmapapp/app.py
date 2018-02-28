695e38a388df8324110a33d1be20207490fdf339
stevenwevans2 <36004196+stevenwevans2@users.noreply.github.com>
2018-02-28 10:53:26 -0700

    Add files via upload

---
+++ tethysapp-stevenevansmapapp/tethysapp/stevenevansmapapp/app.py
@@ -0,0 +1,60 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class Stevenevansmapapp(TethysAppBase):
+    """
+    Tethys app class for Stevenevansmapapp.
+    """
+
+    name = 'Stevenevansmapapp'
+    index = 'stevenevansmapapp:home'
+    icon = 'stevenevansmapapp/images/icon.gif'
+    package = 'stevenevansmapapp'
+    root_url = 'stevenevansmapapp'
+    color = '#2c3e50'
+    description = 'Place a brief description of your app here.'
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='stevenevansmapapp',
+                controller='stevenevansmapapp.controllers.home'
+            ),
+            UrlMap(
+                name='map_view',
+                url='map-view',
+                controller='stevenevansmapapp.controllers.map_view'
+            ),
+            UrlMap(
+                name='data_services',
+                url='data-services',
+                controller='stevenevansmapapp.controllers.data_services'
+            ),
+            UrlMap(
+                name='about',
+                url='about',
+                controller='stevenevansmapapp.controllers.about'
+            ),
+            UrlMap(
+                name='proposal',
+                url='proposal',
+                controller='stevenevansmapapp.controllers.proposal'
+     	    ),
+            UrlMap(
+                name='mockups',
+                url='mockups',
+                controller='stevenevansmapapp.controllers.mockups'
+     	    ),
+        )
+
+        return url_maps
