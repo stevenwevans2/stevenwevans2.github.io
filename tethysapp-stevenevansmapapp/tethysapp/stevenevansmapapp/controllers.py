423d5c0851daee2228bef560f081c5b07ee4a97a
jaredlillywhite <jaredclillywhite@gmail.com>
2018-03-22 08:59:12 -0600

    attempted to add second service

--- tethysapp-stevenevansmapapp/tethysapp/stevenevansmapapp/controllers.py
+++ tethysapp-stevenevansmapapp/tethysapp/stevenevansmapapp/controllers.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from tethys_sdk.gizmos import Button, RangeSlider, LinePlot
+from tethys_sdk.gizmos import *
 
 @login_required()
 def home(request):
@@ -222,3 +223,109 @@ def mockups(request):
     context={}
     return render(request, 'stevenevansmapapp/mockups.html', context)
 
+
+@login_required()
+def costpath(request):
+    """
+    Controller for the app home page.
+    """
+
+    toggledem = ToggleSwitch(display_text='Styled Toggle',
+                                        name='toggledem',
+                                        on_label='Yes',
+                                        off_label='No',
+                                        on_style='success',
+                                        off_style='danger',
+                                        initial=True,
+                                        size='large',
+                                     )
+
+
+    toggle_switch_mun = ToggleSwitch(display_text='Styled Toggle',
+                                        name='togglemun',
+                                        on_label='Yes',
+                                        off_label='No',
+                                        on_style='success',
+                                        off_style='danger',
+                                        initial=True,
+                                        size='large',
+                                     )
+
+    dimensionsbut = Button(display_text='Update Dimensions',
+                             name='dimensions',
+                             style='',
+                             icon='',
+                             href='',
+                             submit=False,
+                             disabled=False,
+                             attributes={"onclick": "dimensionmodal()"},
+                             classes=''
+                        )
+
+    calculate = Button(display_text='Calculate Quantities',
+                             name='calculate',
+                             style='',
+                             icon='',
+                             href='',
+                             submit=False,
+                             disabled=False,
+                             attributes={},
+                             classes=''
+                       )
+
+    results = Button(display_text='View Results',
+                             name='results',
+                             style='',
+                             icon='',
+                             href='',
+                             submit=False,
+                             disabled=False,
+                             attributes={"onclick": "resultmodal()"},
+                             classes=''
+                       )
+
+    dimension_edit = TableView(column_names=('Dimension', 'Value (ft)'),
+                                rows=[('width top (wt)', 7),
+                                      ('width bottom (wb)', 5),
+                                      ('depth (d)', 10),
+                                      ('pipe zone depth (pd)', 2),
+                                      ('pipe diameter (dia)', 1),
+                                      ],
+                                hover=True,
+                                striped=True,
+                                bordered=True,
+                                condensed=True,
+                                editable_columns=(False, 'Value (ft)'),
+                                row_ids=[21, 25, 31]
+                               )
+
+
+    resulttable = TableView(column_names=('Quantity', 'Value (CY)'),
+                                rows=[('Offhaul', 500),
+                                      ('Bedding Import', 150),
+                                      ('Backfill Import', 300),
+                                      ],
+                                hover=True,
+                                striped=True,
+                                bordered=True,
+                                condensed=True
+                            )
+
+    percent_input = TextInput(display_text='Split Percentage (i.e. 5% = 20 points)',
+                           name='percentinput',
+                           placeholder='',
+                           prepend='%',
+                            )
+
+    context = {
+        'toggledem':toggledem,
+        'toggle_switch_mun': toggle_switch_mun,
+        'dimensionsbut':dimensionsbut,
+        'calculate': calculate,
+        'results': results,
+        'dimension_edit': dimension_edit,
+        'resulttable': resulttable,
+        'percent_input': percent_input
+    }
+
+    return render(request, 'stevenevansmapapp/costpath.html', context)
--- tethysapp-stevenevansmapapp/tethysapp/stevenevansmapapp/public/js/main.js
+++ tethysapp-stevenevansmapapp/tethysapp/stevenevansmapapp/public/js/main.js
@@ -2,3 +2,150 @@ function calculate(){
     alert("Calculating the ultimate bike route path!")
     }
 
+init_map_slope = function(){
+        require([
+      "esri/Map",
+      "esri/views/MapView",
+      "esri/widgets/Sketch/SketchViewModel",
+      "esri/Graphic",
+      "esri/layers/GraphicsLayer",
+      "esri/tasks/Geoprocessor",
+      "esri/tasks/support/FeatureSet",
+      "esri/geometry/Point",
+      "dojo/domReady!"
+    ], function(Map, MapView, SketchViewModel, Graphic, GraphicsLayer, Geoprocessor, FeatureSet, Point) {
+        var tempGraphicsLayer = new GraphicsLayer();
+
+        var map = new Map({
+        basemap: "gray",
+        layers: [tempGraphicsLayer],
+        });
+
+
+        var view = new MapView({
+        container: "mapslope",
+        map: map,
+        center: [-111.7,40.25],
+        zoom: 13
+        });
+
+        var markerSymbol = {
+          type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+          color: [255, 0, 0],
+          outline: { // autocasts as new SimpleLineSymbol()
+            color: [255, 255, 255],
+            width: .001
+          }
+        };
+
+        var polylineSymbol = {
+            type: "simple-line",
+            color: "#8B2AE2",
+            width: "4",
+            style: "solid"
+        }
+        var gpUrl = "http://geoserver2.byu.edu/arcgis/rest/services/StorminMormons/SurfaceInfo2/GPServer/Add%20Surface%20Information";
+
+
+        // create a new Geoprocessor
+        var gp = new Geoprocessor(gpUrl);
+        // define output spatial reference
+        gp.outSpatialReference = { // autocasts as new SpatialReference()
+              wkid: 26912 //EPSG3857
+            };
+
+        view.when(function() {
+        // create a new sketch view model
+        var sketchViewModel = new SketchViewModel({
+          view: view,
+          layer: tempGraphicsLayer,
+          polylineSymbol: { // symbol used for polylines
+            type: "simple-line", // autocasts as new SimpleMarkerSymbol()
+            color: "#8A2BE2",
+            width: "3",
+            style: "solid"
+          }
+        });
+
+        // ************************************************************
+        // Get the completed graphic from the event and add it to view.
+        // This event fires when user presses
+        //  * "C" key to finish sketching point, polygon or polyline.
+        //  * Double-clicks to finish sketching polyline or polygon.
+        //  * Clicks to finish sketching a point geometry.
+        // ***********************************************************
+        sketchViewModel.on("draw-complete", function(evt) {
+          tempGraphicsLayer.add(evt.graphic);
+          setActiveButton();
+          var inputGraphicContainer = [];
+          inputGraphicContainer.push(evt.graphic);
+          var featureSet = new FeatureSet();
+          featureSet.features = inputGraphicContainer;
+          // input parameters
+          var params = {
+            "Input_Feature_Class": featureSet,
+          };
+
+          gp.submitJob(params).then(completeCallback, errBack, statusCallback);
+
+        });
+
+        function completeCallback(result){
+
+            gp.getResultData(result.jobId, "output_polyline").then(drawResult, drawResultErrBack);
+
+        }
+
+        function drawResult(data){
+            console.log("test1");
+            var polyline_feature = data.value.features[0];
+                polyline_feature.symbol = polylineSymbol;
+                tempGraphicsLayer.add(polyline_feature);
+
+        }
+
+        function drawResultErrBack(err) {
+            console.log("draw result error: ", err);
+        }
+
+        function statusCallback(data) {
+            console.log(data.jobStatus);
+        }
+        function errBack(err) {
+            console.log("gp error: ", err);
+        }
+
+        var drawLineButton = document.getElementById("polylineButton");
+        drawLineButton.onclick = function() {
+          // set the sketch to create a polyline geometry
+          sketchViewModel.create("polyline");
+          setActiveButton(this);
+        };
+
+
+        document.getElementById("resetBtn").onclick = function() {
+          tempGraphicsLayer.removeAll();
+          sketchViewModel.reset();
+          setActiveButton();
+        };
+
+        function setActiveButton(selectedButton) {
+          // focus the view to activate keyboard shortcuts for sketching
+          view.focus();
+          var elements = document.getElementsByClassName("active");
+          for (var i = 0; i < elements.length; i++) {
+            elements[i].classList.remove("active");
+          }
+          if (selectedButton) {
+            selectedButton.classList.add("active");
+          }
+        }
+      });
+    }
+
+)};
+
+
+$(function() {
+    init_map_slope();
+});
